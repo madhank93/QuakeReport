e232e5b3c557aa70fa165baaa1ad7c3bbaf36091
madhank93 <maddymadhan946@gmail.com>
2019-01-01 12:48:52 +0530

    Implementation of Network connectivity manager

--- app/src/main/java/com/madhank93/android/quakereport/EarthQuakeLoader.java
+++ app/src/main/java/com/madhank93/android/quakereport/EarthQuakeLoader.java
@@ -2,11 +2,14 @@ package com.madhank93.android.quakereport;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.util.Log;
 
 import java.util.List;
 
 public class EarthQuakeLoader extends AsyncTaskLoader<List<EarthQuake>> {
 
+    /** Tag for the log messages */
+    public static final String LOG_TAG = EarthQuakeLoader.class.getSimpleName();
     String url;
 
     public EarthQuakeLoader(Context context,String url) {
@@ -24,6 +27,7 @@ public class EarthQuakeLoader extends AsyncTaskLoader<List<EarthQuake>> {
     @Override
     public List<EarthQuake> loadInBackground() {
 
+        Log.i(LOG_TAG, "Loader background job");
         // Don't perform the request if there are no URLs, or the first URL is null.
         if (url == null) {
             return null;
